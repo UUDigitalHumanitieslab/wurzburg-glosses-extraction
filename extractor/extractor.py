9e7a85442ae47da01039cb0bfc3f96f08068acde
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2015-11-05 09:19:37 +0100

    Added noun extraction

--- extractor/extractor.py
+++ extractor/extractor.py
@@ -1,9 +1,59 @@
 import codecs
 import re
 
-from models import Locus
+from models import Noun, FormAnalysis, Form, Locus
 
-LOCI = re.compile(r'\s(\d+)([a-d]?)(\d*)([a-d]?)(?:\s\((\w+)\))?')
+NOUN = re.compile(r'(.*)\s([nmf])\.\s(.*?)\.')
+FORM_ANALYSIS = re.compile(r'([NVAGD](?:sg|pl|du)).\s')
+FORM = re.compile(r'([^\W\d_]+)\s', re.U)
+LOCI = re.compile(r'(\d+)([a-d]?)(\d*)([a-d]?)(?:\s\((\w+)\))?')
+
+
+def create_noun(s):
+    """
+    Creates a Noun from a string s. A Noun consists of one or more FormAnalyses.
+    """
+    current_noun = None
+    current_form = None
+    for n, match in enumerate(FORM_ANALYSIS.split(s)):
+        if n == 0:
+            current_noun = extract_noun(match)
+        elif n % 2 == 1:
+            current_form = FormAnalysis(match)
+            current_noun.add_form_analyses(current_form)
+        else:
+            current_form.set_forms(extract_forms(match))
+
+    return current_noun
+
+
+def extract_noun(s):
+    """
+    Extracts a single noun from a string s.
+    """
+    match = NOUN.match(s)
+    title = match.group(1)
+    gender = match.group(2)
+    stem = match.group(3)
+    return Noun(title, gender, stem, '', '')
+
+
+def extract_forms(s):
+    """
+    Extracts all forms from a string s. A form consists of a definition and one or more Loci.
+    """
+    forms = []
+    current_form = None
+    for n, match in enumerate(FORM.split(s.decode('utf-8'))):
+        if n == 0:
+            pass  # first match is empty
+        elif n % 2 == 1:
+            current_form = Form(match)
+        else:
+            current_form.set_loci(extract_loci(match))
+            forms.append(current_form)
+
+    return forms
 
 
 def extract_loci(s):
@@ -34,7 +84,11 @@ def extract_loci(s):
     return loci
 
 if __name__ == "__main__":
-    with codecs.open('../data/test1.txt', 'rb') as in_file:
+    #with codecs.open('../data/test1.txt', 'rb') as in_file:
+    #    for line in in_file:
+    #        line = line.strip()
+    #        extract_loci(line)
+    with codecs.open('../data/dan.txt', 'rb') as in_file:
         for line in in_file:
             line = line.strip()
-            extract_loci(line)
+            print create_noun(line)
--- extractor/models.py
+++ extractor/models.py
@@ -1,3 +1,52 @@
+class Noun:
+    def __init__(self, title, gender, stem, additional, definition):
+        self.title = title
+        self.gender = gender
+        self.stem = stem
+        self.additional = additional
+        self.definition = definition
+        self.form_analyses = []
+
+    def add_form_analyses(self, form_analyses):
+        self.form_analyses.append(form_analyses)
+
+    def __str__(self):
+        s = 'Noun: {}, gender: {}, stem: {}'.format(self.title, self.gender, self.stem)
+        for form_analysis in self.form_analyses:
+            s += '\n\t{}'.format(form_analysis)
+        return s
+
+
+class FormAnalysis:
+    def __init__(self, label):
+        self.label = label
+        self.forms = []
+
+    def set_forms(self, forms):
+        self.forms = forms
+
+    def __str__(self):
+        s = 'FormAnalysis: {}'.format(self.label)
+        for form in self.forms:
+            s += '\n\t\t{}'.format(form)
+        return s
+
+
+class Form:
+    def __init__(self, form):
+        self.form = form
+        self.loci = []
+
+    def set_loci(self, loci):
+        self.loci = loci
+
+    def __str__(self):
+        s = 'Form: {}'.format(self.form.encode('utf-8'))
+        for loci in self.loci:
+            s += '\n\t\t\t{}'.format(loci)
+        return s
+
+
 class Locus:
     OCCURRENCES_MAP = {'': 1, 'bis': 2, 'ter': 3, 'quatter': 4}
 
@@ -15,4 +64,4 @@ class Locus:
             raise ValueError('Unknown number of occurrences: {}'.format(nr_occurrences))
 
     def __str__(self):
-        return 'LOCUS p: {}, c: {}, n: {}, s: {}, nr: {}'.format(self.page, self.column, self.number, self.subdivision, self.nr_occurrences)
+        return 'Locus: page: {}, column: {}, number: {}, subdivision: {}, nr_occurrences: {}'.format(self.page, self.column, self.number, self.subdivision, self.nr_occurrences)
