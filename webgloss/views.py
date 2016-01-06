85bbe737e470efca8d4b25611ea052ead72d54e5
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2016-01-06 16:06:01 +0100

    Improved on the web application

--- webgloss/views.py
+++ webgloss/views.py
@@ -1,5 +1,10 @@
 from flask import Blueprint, request, render_template
 
+from extractor.models import Noun, Adjective
+from extractor.advextractor import create_adverb
+from extractor.extractor import create_pos
+from extractor.prepextractor import create_preposition
+from extractor.defartextractor import create_definite_article
 from extractor.verbextractor import create_verb
 
 site = Blueprint('site', __name__)
@@ -9,8 +14,20 @@ site = Blueprint('site', __name__)
 def home():
     """Renders the home view. """
     if request.method == 'POST':
+        pos = request.form['pos']
         s = request.form['gloss'].encode('utf-8')
-        verb = create_verb(s)
-        return render_template('results.html', results=verb.headword.decode('utf-8'))
+        if pos == 'noun':
+            pos = create_pos(s, Noun)
+        if pos == 'adj':
+            pos = create_pos(s, Adjective)
+        if pos == 'verb':
+            pos = create_verb(s)
+        if pos == 'adv':
+            pos = create_adverb(s)
+        if pos == 'prep':
+            pos = create_preposition(s)
+        if pos == 'art':
+            pos = create_definite_article(s)
+        return render_template('results.html', pos=pos)
     else:
         return render_template('home.html')
