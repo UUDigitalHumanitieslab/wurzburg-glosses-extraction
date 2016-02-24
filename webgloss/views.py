8cb2346f4a10b628c0905ea8203837bd45bea944
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2016-02-24 21:55:27 +0100

    Small improvement on Flask microapp

--- webgloss/views.py
+++ webgloss/views.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, request, render_template
 
-from extractor.models import Noun, Adjective
 from extractor.advextractor import create_adverb
 from extractor.extractor import create_pos
 from extractor.prepextractor import create_preposition
@@ -16,17 +15,15 @@ def home():
     if request.method == 'POST':
         pos = request.form['pos']
         s = request.form['gloss'].encode('utf-8')
-        if pos == 'noun':
-            pos = create_pos(s, Noun)
-        if pos == 'adj':
-            pos = create_pos(s, Adjective)
-        if pos == 'verb':
+        if pos == 'noun' or pos == 'adjective':
+            pos = create_pos(s)
+        elif pos == 'verb':
             pos = create_verb(s)
-        if pos == 'adv':
+        elif pos == 'adv':
             pos = create_adverb(s)
-        if pos == 'prep':
+        elif pos == 'prep':
             pos = create_preposition(s)
-        if pos == 'art':
+        elif pos == 'art':
             pos = create_definite_article(s)
         return render_template('results.html', pos=pos)
     else:
